60cef37b4443b9ef5d563e687043a16251657ace
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-11 22:03:46 -0300

    segunda version que si funciona y java 23

--- src/main/java/com/modulocinco/controladores/Formulario.java
+++ src/main/java/com/modulocinco/controladores/Formulario.java
@@ -16,7 +16,7 @@ import java.util.List;
 @WebServlet("/Formulario")
 public class Formulario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 		// Lista de arreglos de Strings, permite una estructura cerrada cada vez
 		private List<String[]> contactos = new ArrayList<>();
        
@@ -38,7 +38,7 @@ public class Formulario extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		//doGet(request, response);
 		
 		// Igual que en los name del formulario
 		String nombre = request.getParameter("nombre");
@@ -53,11 +53,24 @@ public class Formulario extends HttpServlet {
 		salida.println("<!DOCTYPE html>");
 		salida.println("<html lang=\"es\">");
 		salida.println("<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Tabla de contactos</title>");
+		salida.println("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\">");
 		salida.println("<style>body {font-family: \"Open Sans\";}</style>");
 		salida.println("</head>");
 		salida.println("<body>");
 		salida.println("<h2>Ingresos</h2>");
-		if(contactos.isEmpty()){
+
+		salida.println("<form action='/GestorContactos/Formulario' method='POST'>");
+		salida.println("<label>");
+		salida.println("<label for=\"nombre\">Nombre:</label>");
+		salida.println("<input type=\"text\" name=\"nombre\" id='nombre' required /><br />");
+		salida.println("<label for=\"apellido\">Apellido:</label>");
+		salida.println("<input type=\"text\" name=\"apellido\" id='apellido' required /><br />");
+		salida.println("<label for=\"usuario\">Usuario:</label>");
+		salida.println("<input type=\"text\" name=\"usuario\" id='usuario' required /><br />");
+		salida.println("<button type=\"submit\">Enviar</button>");
+		salida.println("</form>");
+
+		if(!contactos.isEmpty()){
 			salida.println("<br />");
 			salida.println("<table border=1>");
 			salida.println("<tr><th>Nombre</th><th>Apellido</th><th>Usuarios</th></tr>");
@@ -72,10 +85,10 @@ public class Formulario extends HttpServlet {
 		
 		}
 		salida.println("</table>");
+		salida.println("<script src=\"app.js\"></script>");
 		salida.println("</body>");
 		salida.println("</html>");
 
-	
 	}
 
 }
